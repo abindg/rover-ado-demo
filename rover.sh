9d506099b5129be82f517f3636984920b6afdc3d
Abin Duttagupta <abin.duttagupta@lexmark.com>
2022-05-31 19:06:26 +0530

    another one change

--- rover.sh
+++ rover.sh
@@ -1,9 +1,5 @@
 sudo mkdir /usr/bin/rover
 curl -L -O https://github.com/im2nguyen/rover/releases/download/v0.3.2/rover_0.3.2_linux_amd64.zip
-sudo cp rover_0.3.2_linux_amd64.zip /usr/bin/rover/
-sudo cd /usr/bin/rover/
 sudo unzip rover_0.3.2_linux_amd64.zip
-export PATH=$PATH:/usr/bin/rover
-sudo cd -
-rover_v0.3.2 -standalone true
+./rover_v0.3.2 -standalone true
 ls -lrt
