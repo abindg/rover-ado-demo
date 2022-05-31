34bc68bfddb87d16d7b124a5573344208c4f5897
Abin Duttagupta <abin.duttagupta@lexmark.com>
2022-05-31 19:22:21 +0530

    added argument

--- rover.sh
+++ rover.sh
@@ -1,5 +1,5 @@
 sudo mkdir /usr/bin/rover
 curl -L -O https://github.com/im2nguyen/rover/releases/download/v0.3.2/rover_0.3.2_linux_amd64.zip
 sudo unzip rover_0.3.2_linux_amd64.zip
-./rover_v0.3.2 -workingDir "$(Pipeline.Workspace)/terraform-artefacts" -standalone true
+./rover_v0.3.2 -workingDir $1 -standalone true
 ls -lrt
