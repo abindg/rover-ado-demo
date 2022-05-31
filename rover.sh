c91c89ac2449006c0caa6c5252640ecd671c3167
Abin Duttagupta <abin.duttagupta@lexmark.com>
2022-05-31 18:59:04 +0530

    chnage in script

--- rover.sh
+++ rover.sh
@@ -3,5 +3,5 @@ curl -L -O https://github.com/im2nguyen/rover/releases/download/v0.3.2/rover_0.3
 sudo cp rover_0.3.2_linux_amd64.zip /usr/bin/rover/
 sudo unzip /usr/bin/rover/rover_0.3.2_linux_amd64.zip
 export PATH=$PATH:/usr/bin/rover
-rover_v0.3.2 -standalone true
+/usr/bin/rover/rover_v0.3.2 -standalone true
 ls -lrt
