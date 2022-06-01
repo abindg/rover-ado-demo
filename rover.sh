02cb5b2f5f537ac2bd3553133ce0d8db01a6362b
Abin Duttagupta <abin.duttagupta@lexmark.com>
2022-06-01 14:31:09 +0530

    lets see

--- rover.sh
+++ rover.sh
@@ -1,4 +1,4 @@
-az login --service-principal -u "33aaa935-be73-441c-800e-03710bcee72c" -p "DXvbHp178UchaK5G.ytSM_RH_9BOyX4k9t" --tenant "12709065-6e6c-41c9-9e4d-fb0a436969ce"
+#az login --service-principal -u "33aaa935-be73-441c-800e-03710bcee72c" -p "DXvbHp178UchaK5G.ytSM_RH_9BOyX4k9t" --tenant "12709065-6e6c-41c9-9e4d-fb0a436969ce"
 sudo mkdir /usr/bin/rover
 curl -L -O https://github.com/im2nguyen/rover/releases/download/v0.3.2/rover_0.3.2_linux_amd64.zip
 sudo unzip rover_0.3.2_linux_amd64.zip
